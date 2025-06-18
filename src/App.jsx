a11712acdc3acaaa9d36ea9b8d3a095f4caae62b
oladunjoye1234 <aolamilekanoladunjoye0412@gmail.com>
2025-06-19 00:45:58 +0100

    my second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Work from './pages/private-pages/work/work'
 import Home from './pages/public-pages/Home/home'
+import ProjectCallToAction from './pages/private-pages/ProjectCallToAction/projectCallToAction'
 // import { Contact } from 'lucide-react'
 
 // import { AuthProvider } from './context/AuthContext'
@@ -16,7 +17,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path= "/work" element={<Work/>} />
-          {/* <Route path="/ProjectCallToAction" element={<Contact />} /> */}
+          <Route path="/Contact" element={<ProjectCallToAction/>} />
         </Routes>
 
 
